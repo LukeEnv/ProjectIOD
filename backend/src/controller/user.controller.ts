c4f4fdf4c229df498179b0052c2ba113aa329bce
Will <wtbeaven@gmail.com>
2025-06-02 21:11:52 +1000

    Added update & delete paths for users

--- backend/src/controller/user.controller.ts
+++ backend/src/controller/user.controller.ts
@@ -3,29 +3,51 @@
 
 import { Request, Response } from "express";
 
-//let userModel = require("../database/schema/user.schema");
 import userSchema from "../database/schema/user.schema";
 import { IUser } from "../database/model/user.model";
 
 export const getUsers = (req: Request, res: Response) => {
-  userSchema
-    .find({})
-    .then((data: IUser[]) => res.send({ result: 200, data: data }))
-    .catch((err: Error) => {
-      console.log(err);
-      res.send({ result: 500, error: err.message });
-    });
+    userSchema
+        .find({})
+        .then((data: IUser[]) => res.send({ result: 200, data: data }))
+        .catch((err: Error) => {
+            console.log(err);
+            res.send({ result: 500, error: err.message });
+        });
 };
 
 export const createUser = (req: Request, res: Response) => {
-  const data: IUser = req.body;
-  console.log(req.body);
-  console.log(data);
-  new userSchema(data)
-    .save()
-    .then((data: IUser) => res.send({ result: 200, data: data }))
-    .catch((err: Error) => {
-      console.log(err);
-      res.send({ result: 500, error: err.message });
-    });
+    const data: IUser = req.body;
+    console.log(req.body);
+    console.log(data);
+    new userSchema(data)
+        .save()
+        .then((data: IUser) => res.send({ result: 200, data: data }))
+        .catch((err: Error) => {
+            console.log(err);
+            res.send({ result: 500, error: err.message });
+        });
+};
+
+export const updateUser = (req: Request, res: Response) => {
+    console.log(req.body);
+    userSchema
+        .findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+        })
+        .then((data) => res.send({ result: 200, data: data }))
+        .catch((err) => {
+            console.log(err);
+            res.send({ result: 500, error: err.message });
+        });
+};
+
+export const deleteUser = (req: Request, res: Response) => {
+    userSchema
+        .findByIdAndDelete(req.params.id)
+        .then((data) => res.send({ result: 200, data: data }))
+        .catch((err) => {
+            console.log(err);
+            res.send({ result: 500, error: err.message });
+        });
 };
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getUsers, createUser } from "../controller/user.controller";
+import { getUsers, createUser, updateUser, deleteUser } from "../controller/user.controller";
 
 const router = Router();
 
 router.get("/", getUsers);
 router.post("/", createUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
 export default router;
